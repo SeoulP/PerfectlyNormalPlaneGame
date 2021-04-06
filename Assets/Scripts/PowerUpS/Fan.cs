4f368834d2ab1e4950f207d004e57288a0c07fe6
SeoulP <55219779+SeoulP@users.noreply.github.com>
2021-04-06 01:34:43 -0500

    All the changes

---
+++ Assets/Scripts/PowerUpS/Fan.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour
+{
+    [SerializeField]
+    PlaneController plane;
+    [SerializeField]
+    float fanPower = 10;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        plane = GameObject.Find("PaperPlane").GetComponent<PlaneController>();
+        rb = plane.GetComponent<Rigidbody>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            rb.AddForce(new Vector3(0, rb.velocity.y, 0) + (Vector3.up * fanPower), ForceMode.VelocityChange);
+        }
+    }
+}
